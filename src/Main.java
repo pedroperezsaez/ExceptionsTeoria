2f7054a917bd9a02f62215231424633ed75c4de3
pedroperezsaez <pedroperezsaez.1992@gmail.com>
2026-05-04 15:18:50 +0200

    exception try catch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 //classe per detectar divisions entre zero
-class DivisioPerZeroException extends  RuntimeException{
+class DivisioPerZeroException extends  Exception{
 
 }
 //clase per detectar divisions 0/0
-class DivisioZeroZeroException extends RuntimeException{
+class DivisioZeroZeroException extends Exception{
 
 }
 public class Main {
@@ -16,14 +16,22 @@ public class Main {
 
             int a= Integer.parseInt(scanner.nextLine());
             int b=Integer.parseInt(scanner.nextLine());
+            while (true){
+                try{
+                    int c= divideix(a,b);
+                    break;
+                } catch(DivisioPerZeroException e){
+                    System.out.println("no puc dividir entre zero");
+                } catch (DivisioZeroZeroException o){
+                    System.out.println("divide 0 entre 0 malo");
+                }
+            }
 
-                int c= divideix(a,b);
-                System.out.println(c);
 
 
     }
         //tambien se puede poner throws Exception en vez d eir poniendo nombre por nombre pero no es lo suyo
-    static int divideix(int a, int b) {
+    static int divideix(int a, int b) throws DivisioPerZeroException,DivisioZeroZeroException {
        if (b==0){
          if (a==0)throw new DivisioZeroZeroException();
          throw new DivisioPerZeroException();
